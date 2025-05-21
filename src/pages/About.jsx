d9e8acb5c4c53410feed8446754bf1a3aeb31671
shameCanales <canalesshame@gmail.com>
2025-05-21 17:33:24 +0800

    completed-career-goals

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,11 @@
+import KnowMeBetter from "../sections/KnowMoreAbout";
+import CareerGoals from "../sections/CareerGoals";
+
 export default function About() {
   return (
     <>
-      <div>
-        <div className="text mt-16">
-          <h2 className="montserrat-extrabold text-xl text-center px-8 ">
-            Get to know me better as you scroll.
-          </h2>
-          <p className="inter-regular text-xs text-center leading-4.5  mt-4">
-            Hello there! I’m Mark Aron Shame Banzuelo Canales and I am an
-            aspiring
-            <span className="font-bold">
-              {" "}
-              Full-Stack Software Engineer
-            </span>{" "}
-            from Campalingo, San Fernando, Romblon. I studied Bachelor of
-            Science in Information Technology at Romblon State University - San
-            Fernando Campus with a passion on Software Engineering.
-          </p>
-        </div>
-      </div>
+      <KnowMeBetter />
+      <CareerGoals />
     </>
   );
 }
--- src/sections/CareerGoals.jsx
+++ src/sections/CareerGoals.jsx
@@ -1,7 +1,21 @@
+import { careerGoals } from "../data";
+import GoalCard from "../components/GoalCard";
+
 export default function CareerGoals() {
   return (
-    <div>
-      <h1>My Career Goals</h1>
+    <div className="mt-12">
+      <h1 className="montserrat-bold text-center">My Career Goals</h1>
+
+      <div className="grid grid-cols-1 gap-4 mt-4">
+        {careerGoals.map((careerGoal) => (
+          <GoalCard
+            key={careerGoal.bgColor}
+            title={careerGoal.goalTitle}
+            description={careerGoal.goalDesc}
+            color={careerGoal.bgColor}
+          />
+        ))}
+      </div>
     </div>
   );
 }
