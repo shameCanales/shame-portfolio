4b2eb53218c18efe1891597c92c583387ece1391
shameCanales <canalesshame@gmail.com>
2025-05-24 23:39:07 +0800

    Refined styling for Tagline not Subheadline

--- src/ui/Tagline.jsx
+++ src/ui/Tagline.jsx
@@ -1,6 +1,6 @@
 export default function Tagline() {
   return (
-    <p className="text-center inter-medium text-xs sm:text-sm sm:text-left sm:w-[460px]">
+    <p className="text-center sm:text-left m-auto sm:m-0 inter-medium text-xs sm:text-sm  w-[300px] ">
       I build accessible, pixel-perfect digital experience for the web,
       Specializing in Frontend Development & NEXT JS.
     </p>
