4604709670190feb7b5e7608bce53f5e51e72eec
shameCanales <canalesshame@gmail.com>
2025-05-24 23:30:48 +0800

    styled techstack for sm viewport

--- src/components/RunningLogosComponent.jsx
+++ src/components/RunningLogosComponent.jsx
@@ -3,7 +3,7 @@ import { logosRow1, logosRow2, logosRow3 } from "../data";
 
 export default function RunningLogosComponent() {
   return (
-    <div className="bg-stone-900 py-4 mt-6">
+    <div className="bg-stone-900 py-4 mt-6 sm:mt-8">
       <InfiniteScrollRow items={logosRow1} direction="left" speed="70s" />
       <InfiniteScrollRow items={logosRow2} direction="right" speed="70s" />
       <InfiniteScrollRow items={logosRow3} direction="left" speed="70s" />
