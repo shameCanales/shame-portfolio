f7efc57a801f23c9230aeba67d3174c72efa4bd7
shameCanales <canalesshame@gmail.com>
2025-05-26 10:04:19 +0800

    styled skillset for viewport sm

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,10 +1,10 @@
 export default function SkillCard({ title, items }) {
   return (
-    <div className="bg-[#07061B] text-stone-50 mt-2 p-4 rounded-md">
-      <p className="text-base poppins-semibold mb-2">{title}</p>
+    <div className="bg-[#07061B] text-stone-50 mt-2 sm:mt-0 p-4 rounded-md sm:min-h-[180px]">
+      <p className="text-base sm:text-xl poppins-semibold mb-2 sm:mb-4">{title}</p>
 
       {items.map((item) => (
-        <p key={item} className="poppins-light text-xs mt-0.5">
+        <p key={item} className="poppins-light text-xs sm:text-sm mt-0.5">
           {item}
         </p>
       ))}
