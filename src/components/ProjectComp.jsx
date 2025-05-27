5c9ca182e06745fa70182e942594f675d731e729
shameCanales <canalesshame@gmail.com>
2025-05-27 10:45:33 +0800

    styled projects section for sm viewport

--- src/components/ProjectComp.jsx
+++ src/components/ProjectComp.jsx
@@ -3,7 +3,7 @@ import Paragraph from "../ui/paragraph";
 
 export default function ProjectComp({ name, desc, src, projFor, stack }) {
   return (
-    <div className="mt-8 mb-14">
+    <div className="mt-8 sm:mt-0 mb-14 sm:mb-4">
       <div className="rounded-xl overflow-hidden aspect-[18/9] border-1 border-stone-700 relative">
         <img className="" src={src} alt={name} />
         <div className="absolute inset-0 hover:bg-black/70 transition-all ease-in-out index-[9]"></div>
@@ -11,7 +11,7 @@ export default function ProjectComp({ name, desc, src, projFor, stack }) {
       <p className="inter-bold text-xs mt-4">{name}</p>
       <p className="uppercase inter-medium text-xs mt-2">{projFor}</p>
       <p className="leading-4.5 tracking-tight inter-regular text-xs mt-2">{desc}</p>
-      <ul className="flex gap-2 mt-2">
+      <ul className="flex gap-1 mt-2">
         {stack.map((tech) => (
           <li key={tech}>
             <TechStackItem item={tech} />
--- src/ui/TechStackItem.jsx
+++ src/ui/TechStackItem.jsx
@@ -1,3 +1,3 @@
 export default function TechStackItem({ item }) {
-  return <p className="px-3 py-1.5 bg-slate-900 text-[#6C61FF] rounded-full text-xs inter-bold">{item}</p>;
+  return <p className="px-2 py-1 bg-slate-900 text-[#6C61FF] rounded-full text-xs inter-bold">{item}</p>;
 }
