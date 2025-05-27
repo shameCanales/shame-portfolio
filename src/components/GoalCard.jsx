ab550a671c2a3526125f3120f79f098b3c2710b1
shameCanales <canalesshame@gmail.com>
2025-05-27 11:54:13 +0800

    styled career goals for sm viewport

--- src/components/GoalCard.jsx
+++ src/components/GoalCard.jsx
@@ -1,9 +1,10 @@
 export default function GoalCard({ title, description, color }) {
+  console.log(color)
   return (
-    <div className={`bg-[${color}] text-stone-50 rounded-md text-center p-5`}>
-      <h3 className="montserrat-bold text-sm tracking-wider">{title}</h3>
+    <div className={`bg-[#463EB1] text-stone-50 rounded-md sm:rounded-xl text-center p-5 sm:py-6 sm:px-14`}>
+      <h3 className="montserrat-bold text-sm sm:text-base tracking-wider">{title}</h3>
 
-      <p className="text-xs montserrat-regular mt-2 leading-relaxed">
+      <p className="text-xs sm:text-sm montserrat-regular mt-2 sm:mt-3 leading-relaxed">
         {description}
       </p>
     </div>
