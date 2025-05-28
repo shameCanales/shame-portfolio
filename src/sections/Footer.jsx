5fc8ce5dd551726115965dcd0ac70d88d9cb141b
shameCanales <canalesshame@gmail.com>
2025-05-28 10:33:53 +0800

    styled footer for sm viewport

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,12 +2,12 @@ import { footerLinks } from "../data";
 
 export default function Footer() {
   return (
-    <div className="mt-4 py-8 px-4 text-center bg-slate-900 text-stone-50">
-      <p className="poppins-light text-xs">DevShame@2025</p>
-      <div className="flex gap-4 mt-4 justify-center">
+    <div className="mt-6 sm:mt-10 py-8 px-4 text-center bg-slate-900 text-stone-50">
+      <p className="poppins-light text-xs sm:text-sm">DevShame@2025</p>
+      <div className="flex gap-4 sm:gap-5 mt-4 justify-center">
         {footerLinks.map((item) => (
           <a href={item.link} key={item.src}>
-            <img className="w-5" src={item.src} alt="" />
+            <img className="w-5 sm:w-6" src={item.src} alt="" />
           </a>
         ))}
       </div>
