9dd2c7c058fd88260a4c344fd5fcf52bb4ce0e67
shameCanales <canalesshame@gmail.com>
2025-05-28 17:41:52 +0800

    styled github for medium viewport

--- src/ui/Paragraph.jsx
+++ src/ui/Paragraph.jsx
@@ -1,7 +1,7 @@
 export default function Paragraph({ children }) {
   return (
     <>
-      <p className="mt-4 leading-4.5 sm:leading-5 tracking-tight text-center inter-medium text-xs sm:text-sm sm:w-[450px] sm:mx-auto">
+      <p className="mt-4 leading-4.5 sm:leading-5 md:leading-6 tracking-tight text-center inter-medium text-xs sm:text-sm md:text-base sm:w-[450px] md:w-[550px] sm:mx-auto">
         {children}
       </p>
     </>
--- src/ui/Subheading.jsx
+++ src/ui/Subheading.jsx
@@ -1,5 +1,5 @@
 export default function Subheading({ children }) {
   return (
-    <p className="text-center montserrat-bold mt-22 sm:text-lg">{children}</p>
+    <p className="text-center montserrat-bold mt-22 md:mt-25 sm:text-lg md:text-xl">{children}</p>
   );
 }
