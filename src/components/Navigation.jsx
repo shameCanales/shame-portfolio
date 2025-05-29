f3358b0eb21ad5d0bf6ac3fa49ae0f657bfb901f
shameCanales <canalesshame@gmail.com>
2025-05-29 17:10:11 +0800

    styled nav for medium viewport

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -98,7 +98,7 @@ export default function Navigation() {
               </LinkText>
             </NavLink>
           </li>
-          
+
           <li>
             <NavLink
               to="/experience"
