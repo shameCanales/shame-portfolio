aa0b34d8b6b2cdcfe4c85bd517b0dcc2ccbfbb53
shameCanales <canalesshame@gmail.com>
2025-05-29 16:10:03 +0800

    styled github for medium viewport

--- src/ui/LogoItem.jsx
+++ src/ui/LogoItem.jsx
@@ -4,8 +4,8 @@ export default function Logoitem({alt, logo, name }) {
   return (
     <>
       <div className="flex items-center justify-center py-0 px-4">
-        <img className="w-4 sm:w-5 object-contain" src={logo} alt={alt} />
-        <p className="ml-4 text-xs sm:text-sm poppins-light text-stone-50">{name}</p>
+        <img className="w-4 sm:w-5 md:w-6 object-contain" src={logo} alt={alt} />
+        <p className="ml-4 text-xs sm:text-sm md:text-base poppins-light text-stone-50">{name}</p>
       </div>
     </>
   );
