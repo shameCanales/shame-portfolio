8248bcb158149b559139a7a8bfdef9076a243e8b
shameCanales <canalesshame@gmail.com>
2025-05-29 11:56:52 +0800

    styled techstack section for medium viewport

--- src/components/InfiniteScrollRow.jsx
+++ src/components/InfiniteScrollRow.jsx
@@ -4,7 +4,8 @@ export default function InfiniteScrollRow({ items, direction, speed = "20s" }) {
   const duplicatedItems = [...items, ...items, ...items, ...items, ...items];
 
   return (
-    <div className="relative overflow-hidden py-2 sm:py-4">
+    <div className="relative overflow-hidden py-2 sm:py-4 md:py-5
+    ">
       <div
         className={`flex gap-8 w-max ${
           direction === "right"
