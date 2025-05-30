0e9394cc77b43fdda6008f5e10eb0b6b65b6ccd4
shameCanales <canalesshame@gmail.com>
2025-05-30 17:12:32 +0800

    styled knowmore for medium VP

--- src/sections/KnowMoreAbout.jsx
+++ src/sections/KnowMoreAbout.jsx
@@ -1,11 +1,11 @@
 export default function KnowMeBetter() {
   return (
     <div>
-      <div className="text mt-16 sm:mt-7">
-        <h2 className="montserrat-extrabold text-xl sm:text-2xl text-center px-8 sm:max-w-[380px] sm:mx-auto">
+      <div className="text mt-16 sm:mt-7 md:mt-8">
+        <h2 className="montserrat-extrabold text-xl sm:text-2xl md:text-3xl text-center px-8 sm:max-w-[380px] sm:mx-auto">
           Get to know me better as you scroll.
         </h2>
-        <p className="inter-regular text-xs sm:text-sm text-center leading-4.5 sm:leading-5 mt-4 sm:mt-6 sm:max-w-[580px] sm:mx-auto">
+        <p className="inter-regular text-xs sm:text-sm md:text-base text-center leading-4.5 sm:leading-5 md:leading-6 mt-4 sm:mt-6 md:mt-8 sm:max-w-[580px] md:max-w-[650px] sm:mx-auto">
           Hello there! I’m Mark Aron Shame Banzuelo Canales and I am an aspiring
           <span className="font-bold"> Full-Stack Software Engineer</span> from
           Campalingo, San Fernando, Romblon. I studied Bachelor of Science in
