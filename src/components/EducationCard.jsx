9178b9c3738c384a2bb977272a6bdd46118bc84d
shameCanales <canalesshame@gmail.com>
2025-05-30 17:37:55 +0800

    styled education for medium viewport

--- src/components/EducationCard.jsx
+++ src/components/EducationCard.jsx
@@ -1,8 +1,8 @@
 export default function EducationCard({ title, year, name, address }) {
   return (
-    <div className="bg-[#07061B] text-white p-4 sm:px-10 sm:py-5 rounded-lg">
-      <h1 className="poppins-bold 5text-sm sm:text-base">{title}</h1>
-      <p className="montserrat-regular text-xs sm:text-sm leading-relaxed mt-2 sm:mt-1">
+    <div className="bg-[#07061B] text-white p-4 sm:px-10 sm:py-5 md:py-7 rounded-lg sm:rounded-xl md:rounded-3xl">
+      <h1 className="poppins-bold 5text-sm sm:text-base md:text-xl">{title}</h1>
+      <p className="montserrat-regular text-xs sm:text-sm leading-relaxed mt-2 md:mt-3 sm:mt-1">
         {name}
         <br />
         {year}
