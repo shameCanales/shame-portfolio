c89456564e8338fd4d417ee234e2ac163bd98b20
shameCanales <canalesshame@gmail.com>
2025-05-30 17:21:39 +0800

    styled career goals for mdVP

--- src/components/GoalCard.jsx
+++ src/components/GoalCard.jsx
@@ -1,10 +1,10 @@
 export default function GoalCard({ title, description, color }) {
   console.log(color)
   return (
-    <div className={`bg-[#463EB1] text-stone-50 rounded-md sm:rounded-xl text-center p-5 sm:py-6 sm:px-14`}>
-      <h3 className="montserrat-bold text-sm sm:text-base tracking-wider">{title}</h3>
+    <div className={`bg-[#463EB1] text-stone-50 rounded-md sm:rounded-xl md:rounded-2xl text-center p-5 sm:py-6 md:py-8 sm:px-14 md:px-8`}>
+      <h3 className="montserrat-bold text-sm sm:text-base md:text-xl tracking-wider">{title}</h3>
 
-      <p className="text-xs sm:text-sm montserrat-regular mt-2 sm:mt-3 leading-relaxed">
+      <p className="text-xs sm:text-sm  montserrat-regular mt-2 sm:mt-3 md:mt-4 leading-relaxed">
         {description}
       </p>
     </div>
--- src/sections/CareerGoals.jsx
+++ src/sections/CareerGoals.jsx
@@ -4,10 +4,10 @@ import Subheading from "../ui/Subheading";
 
 export default function CareerGoals() {
   return (
-    <div className="mt-12">
+    <div className="mt-12 md:mt-14">
       <Subheading>My Career Goals</Subheading>
 
-      <div className="grid grid-cols-1 gap-4  mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4  mt-4 md:mt-8">
         {careerGoals.map((careerGoal) => (
           <GoalCard
             key={careerGoal.goalTitle}
