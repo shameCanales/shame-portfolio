ee8b08ba03041526d4dcb10b19f4198a78360e63
shameCanales <canalesshame@gmail.com>
2025-06-01 23:37:44 +0800

    styled projects for large vp

--- src/components/ProjectComp.jsx
+++ src/components/ProjectComp.jsx
@@ -3,21 +3,25 @@ import Paragraph from "../ui/paragraph";
 
 export default function ProjectComp({ name, desc, src, projFor, stack }) {
   return (
-    <div className="mt-8 sm:mt-0 mb-14 sm:mb-4">
-      <div className="rounded-xl overflow-hidden aspect-[15/9] border-1 border-stone-700 relative">
+    <div className="mt-8 sm:mt-0 mb-14 sm:mb-4 lg:mb-2 lg:grid lg:grid-cols-2 items-center justify-center lg:gap-10">
+      <div className="rounded-xl overflow-hidden aspect-[15/9] border-1 border-stone-700 relative lg:w-[340px] justify-self-end">
         <img className="" src={src} alt={name} />
         <div className="absolute inset-0 hover:bg-black/70 transition-all ease-in-out index-[9]"></div>
       </div>
-      <p className="inter-bold text-xs mt-4">{name}</p>
-      <p className="uppercase inter-medium text-xs mt-2">{projFor}</p>
-      <p className="leading-4.5 tracking-tight inter-regular text-xs mt-2">{desc}</p>
-      <ul className="flex gap-1 mt-2 md:gap-2">
-        {stack.map((tech) => (
-          <li key={tech}>
-            <TechStackItem item={tech} />
-          </li>
-        ))}
-      </ul>
+      <div className="lg:max-w-[447px]">
+        <p className="inter-bold text-xs lg:text-base mt-4 lg:mt-0">{name}</p>
+        <p className="uppercase inter-medium text-xs lg:text-base mt-2">{projFor}</p>
+        <p className="leading-4.5 lg:leading-6 tracking-tight inter-regular text-xs lg:text-base mt-2">
+          {desc}
+        </p>
+        <ul className="flex gap-1 mt-2 lg:mt-4 md:gap-2">
+          {stack.map((tech) => (
+            <li key={tech}>
+              <TechStackItem item={tech} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectComp from "../components/ProjectComp";
 
 export default function Projects() {
   return (
-    <div className="mt-8 sm:mt-02 sm:grid sm:grid-cols-2 sm:gap-5 md:gap-6">
+    <div className="mt-8 sm:mt-2 lg:mt-10 sm:grid sm:grid-cols-2 lg:grid-cols-1 sm:gap-5 md:gap-6">
       {projects.map((proj) => (
         <ProjectComp
           key={proj.name}
