bff5d2432d606727b2fd1e9681838946cd6b5d05
shameCanales <canalesshame@gmail.com>
2025-06-01 22:38:35 +0800

    styled skillset for large viewport

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -3,7 +3,7 @@ import SkillCard from "../components/SkillCard";
 
 export default function Skills() {
   return (
-    <div className="grid mt-4 sm:grid-cols-2 sm:gap-3">
+    <div className="grid mt-4 sm:mt-6 md:mt-8 lg:mt-10 sm:grid-cols-2 lg:grid-cols-3 sm:gap-3">
       {skillsets.map((set) => (
         <SkillCard key={set.name} title={set.name} items={set.items} />
       ))}
