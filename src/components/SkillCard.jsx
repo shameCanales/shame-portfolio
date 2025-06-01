938365aebe8e3895b1e0b3e2e567f0a0f5deb8f2
shameCanales <canalesshame@gmail.com>
2025-06-01 22:49:49 +0800

    fix conflicts

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,6 +1,6 @@
 export default function SkillCard({ title, items }) {
   return (
-    <div className="bg-[#07061B] text-stone-50 mt-2 sm:mt-0 p-4 sm:p-6 md:p-8 lg:px-10 lg:py-6 rounded-md sm:rounded-xl md:rounded-2xl lg:rounded-3xl sm:min-h-[180px]">
+    <div className="bg-[#07061B] text-stone-50 mt-2 sm:mt-0 p-4 sm:p-6 md:p-8 lg:py-6 rounded-md sm:rounded-xl md:rounded-2xl lg:rounded-3xl sm:min-h-[180px]">
       <p className="text-base sm:text-xl poppins-semibold mb-2 sm:mb-3">{title}</p>
 
       {items.map((item) => (
