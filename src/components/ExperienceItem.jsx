3bc405a243c795219af1b1167e1556aa83d5e807
shameCanales <canalesshame@gmail.com>
2025-06-02 09:44:04 +0800

    styled experience section for large viewport

--- src/components/ExperienceItem.jsx
+++ src/components/ExperienceItem.jsx
@@ -1,12 +1,12 @@
 export default function ExperienceItem({ when, as, where, what }) {
   return (
     <>
-      <div className="mt-7 md:mt-9">
-        <div>
-          <p className="poppins-semibold text-lg md:text-xl uppercase" >{when}</p>
+      <div className="mt-7 md:mt-9 lg:mt-4 lg:grid lg:grid-cols-2">
+        <div className="lg:px-6 lg:mt-3">
+          <p className="poppins-semibold text-lg md:text-xl uppercase lg:text-right" >{when}</p>
         </div>
         <div className="px-3 sm:px-5 md:px-6 mt-3 border-l-2 border-l-stone-600" >
-          <p className="poppins-semibold text-sm sm:text-base md:text-lg ">
+          <p className="poppins-semibold text-sm sm:text-base md:text-lg lg:text-xl">
             {as}, {where}
           </p >
           <p className="montserrat-regular text-xs sm:text-sm md:text-base leading-4.5 sm:leading-5 md:leading-6 tracking-tight mt-2">{what}</p>
