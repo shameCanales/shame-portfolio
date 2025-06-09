bc763e3aa0e8af8ae14cd4bf8f558e23491d7a61
shameCanales <canalesshame@gmail.com>
2025-06-09 18:40:16 +0800

    styled project for extra large viewport

--- src/ui/TechStackItem.jsx
+++ src/ui/TechStackItem.jsx
@@ -1,3 +1,3 @@
 export default function TechStackItem({ item }) {
-  return <p className="px-2 py-1 bg-slate-900 text-[#6C61FF] rounded-full text-xs lg:text-sm inter-bold">{item}</p>;
+  return <p className="px-2 py-1 bg-slate-900 text-[#6C61FF] rounded-full text-xs lg:text-sm xl:text-base inter-bold">{item}</p>;
 }
