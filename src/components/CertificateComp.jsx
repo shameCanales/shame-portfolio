a269712537f5c5e1cf7f137706615e631e429392
shameCanales <canalesshame@gmail.com>
2025-06-09 18:27:57 +0800

    styled certificates for extra large viewport

--- src/components/CertificateComp.jsx
+++ src/components/CertificateComp.jsx
@@ -6,8 +6,8 @@ export default function CertificateComp({ name, certBy, src, platform, time }) {
         <div className="absolute inset-0 hover:bg-black/70 transition-all ease-in-out"></div>
       </div>
 
-      <p className="poppins-semibold text-sm mt-4">{name}</p>
-      <p className="poppins-regular text-sm mt-1">{certBy}</p>
+      <p className="poppins-semibold text-sm xl:text-base mt-4">{name}</p>
+      <p className="poppins-regular text-sm xl:text-base mt-1">{certBy}</p>
       {/* <p className="poppins- text-sm mt-1 uppercase">
         {platform} - {time} HOURS
       </p> */}
