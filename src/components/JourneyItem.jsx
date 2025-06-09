28ec451406d77788e0f7efcc5eea9c6dd5e48629
shameCanales <canalesshame@gmail.com>
2025-06-09 19:28:36 +0800

    styled developer journey for extra large viewport

--- src/components/JourneyItem.jsx
+++ src/components/JourneyItem.jsx
@@ -3,10 +3,10 @@ export default function JourneyItem({ title, desc, color }) {
   return (
     <div className={`mt-4 border-l-[${color}] border-l-3 lg:border-l-0 pl-4 lg:pl-0 lg:grid lg:grid-cols-2 `}>
       <div>
-        <p className="montserrat-medium text-sm sm:text-base lg:text-right lg:pr-4">{title}</p>
+        <p className="montserrat-medium text-sm sm:text-base xl:text-xl lg:text-right  lg:pr-4">{title}</p>
       </div>
       <div>
-        <p className="montserrat-regular text-xs sm:text-sm leading-normal mt-1.5 lg:mt-0 lg:border-l-3 lg:pl-4">
+        <p className="montserrat-regular text-xs sm:text-sm xl:text-base leading-normal mt-1.5 lg:mt-0 lg:border-l-3 lg:pl-4">
           {desc}
         </p>
       </div>
