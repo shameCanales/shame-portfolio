45da6f0321eb85f07356a8884f87500448892c04
shameCanales <canalesshame@gmail.com>
2025-06-11 21:31:34 +0800

    styled github for 2xl viewport

--- src/ui/Paragraph.jsx
+++ src/ui/Paragraph.jsx
@@ -1,7 +1,7 @@
 export default function Paragraph({ children }) {
   return (
     <>
-      <p className="mt-4 leading-4.5 sm:leading-5 md:leading-6 tracking-tight text-center inter-medium text-xs sm:text-sm md:text-base sm:w-[450px] md:w-[550px] sm:mx-auto">
+      <p className="mt-4 2xl:mt-6 leading-4.5 sm:leading-5 md:leading-6 2xl:leading-7 tracking-tight text-center inter-medium text-xs sm:text-sm md:text-base 2xl:text-lg sm:w-[450px] md:w-[550px] 2xl:w-[650px] sm:mx-auto">
         {children}
       </p>
     </>
--- src/ui/Subheading.jsx
+++ src/ui/Subheading.jsx
@@ -1,5 +1,5 @@
 export default function Subheading({ children }) {
   return (
-    <p className="text-center montserrat-bold mt-22 md:mt-25 lg:mt-30 sm:text-lg md:text-xl lg:text-2xl">{children}</p>
+    <p className="text-center montserrat-bold mt-22 md:mt-25 lg:mt-30 2xl:mt-40 sm:text-lg md:text-xl lg:text-2xl 2xl:text-3xl">{children}</p>
   );
 }
