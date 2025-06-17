7ceaebb30ee7a6e1af18175eaae73602f0434722
shameCanales <canalesshame@gmail.com>
2025-06-17 17:00:26 +0800

    styled skillset for 2xl viewport

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -17,7 +17,7 @@ export default function RootLayout() {
         <Github />
       </div>
       <TechStack />
-      <div className="px-5 sm:px-8 md:px-13 xl:px-30">
+      <div className="px-5 sm:px-8 md:px-13 xl:px-30 2xl:max-w-[1630px] 2xl:mx-auto">
         <Navigation />
         <Outlet />
       </div>
