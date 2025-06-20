c25952350c55760a8ca0706e5295f9c63bcbaed5
shameCanales <canalesshame@gmail.com>
2025-06-20 23:11:54 +0800

    styled education for 2xl viewport

--- src/sections/Education.jsx
+++ src/sections/Education.jsx
@@ -7,7 +7,7 @@ export default function Education() {
     <div className="">
       <Subheading>Educational Background</Subheading>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-4 sm:gap-3 mt-4 sm:mt-6 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 sm:gap-3 lg:gap-4 2xl:gap-6 mt-4 sm:mt-6 2xl:mt-12 2xl:mx-auto 2xl:max-w-[1180px]">
         {education.map((educ) => (
           <EducationCard
             key={educ.schoolYear}
