e5cc1ea2a53a54ab88fda828375894a80b1b86c7
shameCanales <canalesshame@gmail.com>
2025-06-20 22:49:56 +0800

    styled knowme section for 2xl viewport

--- src/sections/KnowMoreAbout.jsx
+++ src/sections/KnowMoreAbout.jsx
@@ -1,13 +1,13 @@
 export default function KnowMeBetter() {
   return (
     <div>
-      <div className="text mt-16 sm:mt-7 md:mt-8 lg:mt-12">
-        <h2 className="montserrat-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl lg:leading-12 text-center px-8 sm:max-w-[380px] sm:mx-auto">
+      <div className="text mt-16 sm:mt-7 md:mt-8 lg:mt-12 2xl:mt-20">
+        <h2 className="montserrat-extrabold text-xl sm:text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl lg:leading-12 2xl:leading-16 text-center px-8 sm:max-w-[380px] 2xl:max-w-[500px] sm:mx-auto">
           Get to know me better as you scroll.
         </h2>
-        <p className="inter-regular text-xs sm:text-sm md:text-base text-center leading-4.5 sm:leading-5 md:leading-6 mt-4 sm:mt-6 md:mt-8 sm:max-w-[580px] md:max-w-[650px] sm:mx-auto">
+        <p className="inter-regular text-xs sm:text-sm md:text-base 2xl:text-lg text-center leading-4.5 sm:leading-5 md:leading-6 2xl:leading-7 mt-4 sm:mt-6 md:mt-8 2xl:mt-16 sm:max-w-[580px] md:max-w-[650px] 2xl:max-w-[780px] sm:mx-auto">
           Hello there! I’m Mark Aron Shame Banzuelo Canales and I am an aspiring
-          <span className="font-bold"> Full-Stack Software Engineer</span> from
+          <span className="font-bold"> Front-end Software Engineer</span> from
           Campalingo, San Fernando, Romblon. I studied Bachelor of Science in
           Information Technology at Romblon State University - San Fernando
           Campus with a passion on Software Engineering.
