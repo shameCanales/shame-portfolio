a2d91c41ede8a54e1f2a48de586938b0f89aabbb
shameCanales <canalesshame@gmail.com>
2025-08-22 12:12:50 +0800

    refactored navigation logic and added framer motion animation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router";
-import LinkText from "../ui/LinkText";
+import { useSelector, useDispatch } from "react-redux";
+import { motion, AnimatePresence } from "motion/react";
 import hamburgerMenu from "../assets/icons/more.png";
 import closeMenu from "../assets/icons/close.png";
-import { useSelector, useDispatch } from "react-redux";
+import LinkText from "../ui/LinkText";
 import { uiActions } from "../store/ui-slice";
 
 export default function Navigation() {
   const location = useLocation();
   const dispatch = useDispatch();
+
+  const navigationalLinksRoute = [
+    {
+      to: "/",
+      label: "Skill Set",
+    },
+    {
+      to: "/certificates",
+      label: "Certificates",
+    },
+    {
+      to: "/projects",
+      label: "Projects",
+    },
+    {
+      to: "/experience",
+      label: "Experience",
+    },
+    {
+      to: "/about",
+      label: "About Me",
+    },
+  ];
 
   const routeNameMap = {
     "/": "Skill Set",
@@ -17,17 +42,23 @@ export default function Navigation() {
     "/about": "About Me",
   };
 
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 640);
+
+  // monitoring screensize
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 640);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const currentSection = routeNameMap[location.pathname] || "wayaGani!";
   const navIsOpen = useSelector((state) => state.ui.navMobileisOpen);
-  const OpenNav = navIsOpen ? "block" : "hidden";
-
-  const handleOpenNav = () => {
-    dispatch(uiActions.openNavMobile());
-  };
-
-  const handleCloseNav = () => {
-    dispatch(uiActions.closeNavMobile());
-  };
+  const shouldShowNav = navIsOpen || isDesktop;
 
   return (
     <div className="mt-20  relative z-[2]">
@@ -37,95 +68,57 @@ export default function Navigation() {
         </p>
 
         <button
-          onClick={handleOpenNav}
+          onClick={() => dispatch(uiActions.openNavMobile())}
           className="bg-[#463EB1] p-2.5 rounded-full"
         >
           <img src={hamburgerMenu} alt="hamburger Menu icon" className="w-5" />
         </button>
       </div>
 
-      <nav
-        className={`bg-stone-900 sm:bg-transparent text-stone-50 sm:text-stone-950 mt-8 lg:mx-auto px-6 py-10 sm:py-4 rounded-3xl w-46 sm:w-full lg:w-[800px] absolute sm:static top-[-32px] right-0 ${OpenNav}  sm:block `}
-      >
-        <div className="w-full flex justify-end sm:hidden">
-          <button
-            onClick={handleCloseNav}
-            className="bg-purple-700 p-2 rounded-full"
+      {/* Navigation */}
+      <AnimatePresence>
+        {shouldShowNav && (
+          <motion.nav
+            key="nav"
+            initial={{ opacity: 0, x: isDesktop ? 0 : 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: isDesktop ? 0 : 50 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className={`bg-stone-900 sm:bg-transparent text-stone-50 sm:text-stone-950 mt-8 lg:mx-auto px-6 py-10 sm:py-4 rounded-3xl w-46 sm:w-full lg:w-[800px] absolute sm:static top-[-32px] right-0  `}
           >
-            <img className="w-3" src={closeMenu} alt="Close Menu Icon" />
-          </button>
-        </div>
+            {!isDesktop && (
+              <div className="w-full flex justify-end sm:hidden">
+                <button
+                  onClick={() => dispatch(uiActions.closeNavMobile())}
+                  className="bg-purple-700 p-2 rounded-full"
+                >
+                  <img className="w-3" src={closeMenu} alt="Close Menu Icon" />
+                </button>
+              </div>
+            )}
 
-        <ul className="text-center gap-8 sm:flex justify-between">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-purple-800 poppins-bold text-sm "
-                  : "poppins-light"
-              }
-              end
-            >
-              <LinkText active={currentSection === "Skill Set"}>
-                Skill Set
-              </LinkText>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/certificates"
-              className={({ isActive }) =>
-                isActive ? "text-purple-800 poppins-bold" : "poppins-light"
-              }
-            >
-              <LinkText active={currentSection === "Certificates"}>
-                Certificates
-              </LinkText>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/projects"
-              className={({ isActive }) =>
-                isActive ? "text-purple-800 poppins-bold  " : "poppins-light"
-              }
-            >
-              <LinkText active={currentSection === "Projects"}>
-                Projects
-              </LinkText>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/experience"
-              className={({ isActive }) =>
-                isActive ? "text-purple-800 poppins-bold " : "poppins-light"
-              }
-            >
-              <LinkText active={currentSection === "Experience"}>
-                Experience
-              </LinkText>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive ? "text-purple-800 poppins-bold " : "poppins-light"
-              }
-            >
-              <LinkText active={currentSection === "About Me"}>
-                About Me
-              </LinkText>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+            <ul className="text-center gap-8 sm:flex justify-between">
+              {navigationalLinksRoute.map((route) => (
+                <li key={route.to}>
+                  <NavLink
+                    to={route.to}
+                    className={({ isActive }) =>
+                      isActive
+                        ? "text-purple-800 poppins-bold text-sm "
+                        : "poppins-light"
+                    }
+                    end
+                  >
+                    <LinkText active={currentSection === route.label}>
+                      {route.label}
+                    </LinkText>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </motion.nav>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
