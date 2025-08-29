7fc5904680f6e93ba8a3661c5bf3c2eab7289d1d
shameCanales <canalesshame@gmail.com>
2025-08-29 18:24:51 +0800

    added max width to the techstack logo container

--- src/components/RunningLogosComponent.jsx
+++ src/components/RunningLogosComponent.jsx
@@ -3,7 +3,7 @@ import { logosRow1, logosRow2, logosRow3 } from "../data";
 
 export default function RunningLogosComponent() {
   return (
-    <div className="bg-stone-900 py-4 md:py-6 lg:py-8 2xl:py-10 mt-6 sm:mt-8 ">
+    <div className="bg-stone-900 py-4 md:py-6 lg:py-8 2xl:py-10 mt-6 sm:mt-8 2xl:mx-auto 2xl:rounded-4xl 2xl:max-w-[1390px]  ">
       <InfiniteScrollRow items={logosRow1} direction="left" speed="70s" />
       <InfiniteScrollRow items={logosRow2} direction="right" speed="70s" />
       <InfiniteScrollRow items={logosRow3} direction="left" speed="70s" />
