ff30eb4773a6452b03c891e53a165c8666390064
shameCanales <canalesshame@gmail.com>
2025-08-29 17:23:50 +0800

    chancged subheadline from full stack to frontend

--- src/ui/SubHeadline.jsx
+++ src/ui/SubHeadline.jsx
@@ -1,7 +1,7 @@
 export default function SubHeadline() {
   return (
     <p className="text-center sm:text-left inter-semibold text-xs sm:text-sm lg:text-base 2xl:text-lg py-3 2xl:mt-3">
-      Aspiring Full Stack Engineer
+      Aspiring Frontend Engineer
     </p>
   );
 }
